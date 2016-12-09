4e91159f7d1f69a4436a783841cd5378f82a69d5
timothe3192 <blanco.timothe@gmail.com>
2016-12-09 22:48:39 +0100

    Add review.php file

---
+++ review.php
@@ -0,0 +1,7 @@
+<?php 
+require_once "requests/config.php";
+require_once "templates/header.php";
+require_once "templates/footer.php";
+?>
+
+<button type="button" class="">Commencer un exercice</button>
