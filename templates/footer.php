708285d816a9bed6eada818e51abf583479c2539
timothe3192 <blanco.timothe@gmail.com>
2016-12-09 22:39:30 +0100

    Add repository + footer + header

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<footer>
+</footer>
+<script src="http://code.responsivevoice.org/responsivevoice.js"></script>
+<script src="../libs/jquery-3.1.1.min.js"></script>
+<script src="../src/scripts/main.js" type="text/javascript" charset="utf-8"></script>
+<script src="../libs/owl.carousel.min.js"></script>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+	<link rel="stylesheet" href="../src/styles/main.css" />
+	<link rel="stylesheet" href="../src/styles/header.css" />
+	<link rel="stylesheet" href="../src/styles/footer.css" />
+	<link rel="stylesheet" href="../src/styles/home.css" />
+	<link rel="stylesheet" href="../src/styles/exercice-1.css" />
+	<!-- Important Owl stylesheet -->
+	<link rel="stylesheet" href="../libs/owl.carousel.css">
+	<!-- Default Theme -->
+	<link rel="stylesheet" href="../libs/owl.theme.css">
+	<!-- Jquery -->
+	<title>Questionnaire interactif en jQuery</title>
+</head>
+<body>
+<header>
+</header>
