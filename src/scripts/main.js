04a58c39c8a6adc7dfa98cba75893dbaef263f1d
timothe3192 <blanco.timothe@gmail.com>
2016-12-09 22:46:19 +0100

    Add new repository + scripts repository + main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+// 	 $( "#r1" ).click(function() {
+//       // var yolo = document.querySelector(".il");
+//   	 	responsiveVoice.speak($('#r1').text());
+// 	 });
+})
+
+function compteur(){
+	for (i = 1; i < 10; i++)
+	
+}
