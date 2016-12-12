cf7576c5b1a64726d7c9545d133859bd429336d0
Axel <axel.morand@hetic.net>
2016-12-12 11:35:36 +0100

    Created new file + fixed exercice structure

---
+++ salut.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Valérian sauve nous !</title>
+</head>
+<body>
+<?php require_once "templates/header.php"; ?>
+
+
+    <div class="question">
+    </div>
+    <div class="answers">
+    </div>
+    <button class="next-question">Next</button>
+
+
+<?php require_once "templates/footer.php"; ?>
+</body>
+</html>
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,117 +1,82 @@
-// $( document ).ready(function() {
+$(document).ready(function() {
 
-//     $( ".test" ).click(function() {
-// 	console.log('toto');
-// 	});
-	
-// });
+    var Session = function(exo) {
+    	var self = this;
+        this.displayQuestion = $('.question');
+        this.displayAnswers = $('.answers');
+        this.nextQuestionElement = $('.next-question');
+        this.nextQuestionElement.click(function(){
+        	self.next_question();
+        });
+        this.result = new Array();
+        this.exercice = exo;
+        this.qCount = 0;
+        this.rand = 0; //Si la session est random
+    }
+
+    Session.prototype.get_exercice_data = function(url)
+    {
+    	var self = this;
+    	return $.ajax({
+    		method:'GET',
+    		url: url,
+    		success: function(data)
+    		{
+    			console.log(data);
+    			self.data = JSON.parse(data);
+    		}
+    	});
+    }
+
+    Session.prototype.get_question = function(exoId, questionId) {
+    	console.log(this.data);
+        return this.data.exercice[exoId].question[questionId].question_info.texte;
+    }
+
+    Session.prototype.get_answer = function(exoId, questionId, answerId, result) {
+        return this.data.exercice[exoId].question[questionId].reponse[answerId].texte;
+    }
+
+    Session.prototype.show_question = function(exoId, questionId) {
+        this.displayQuestion.text(this.get_question(exoId, questionId)+'');
+    }
+
+    Session.prototype.show_answers = function(exoId, questionId) {
+        var numberOfAnswers = this.data.exercice[exoId].question[questionId].reponse.length;
+        for (var i = 0; i < numberOfAnswers; i++) {
+            this.displayAnswers.append('<button class="answer">' + this.get_answer(exoId, questionId, i) + '</button>');-
+        }
+    }
+
+    Session.prototype.clean_answers = function() 
+    {
+    	this.displayAnswers.html('');
+    }
+
+    Session.prototype.next_question = function() 
+    {
+    	this.clean_answers();
+    	this.qCount +=1;
+    	this.show_question(this.exercice, this.qCount);
+    	this.show_answers(this.exercice, this.qCount);
+    }
+
+    Session.prototype.check_question = function()
+    {
+    	console.log('toto');
+    }
+
+    var sessionExo1 = new Session(0);
+    sessionExo1.get_exercice_data('exercice-1.php').done(
+	    function() {
+	    	sessionExo1.show_question(0,0);
+	    	sessionExo1.show_answers(0,0);
+	    }
+    );
+
+});
+
 // // 	 $( "#r1" ).click(function() {
 // //       // var yolo = document.querySelector(".il");
 // //   	 	responsiveVoice.speak($('#r1').text());
 // // 	 });
-// {
-// 	"exercice": 	[
-// 		{
-// 			"question":
-// 				[
-// 				{
-// 					"question_info":
-// 					{"id":0, "texte":"Quelle est la couleur du cheval blanc d'henri huit", "exercice":1}
-// 					,
-// 					"reponse":
-// 					[
-// 						{
-// 							"id":0,"texte":"Blanc","q_id":0,"result":1
-// 						},
-// 						{
-// 							"id":1,"texte":"Noir","q_id":0,"result":0
-// 						}
-// 					]
-// 					"totalRep":2
-// 				},
-// 				{
-// 					"id":1, "texte":"Comment diagonaliser une matrice de couplage d'oscillateur harmonique?", "exercice":1,
-// 					"reponse":
-// 					[
-// 						{
-// 							"id":0,"texte":"J'ai pas appris mon cours","q_id":0,"result":0
-// 						},
-// 						{
-// 							"id":1,"texte":"On cherche les valeures propres puis les vecteurs propres etc","q_id":0,"result":1
-// 						}
-// 					],
-// 					"totalRep":2
-// 				}
-// 				]
-
-// 		}
-// 	],
-// 	"totalQ":2,
-// 	"totalEx":1
-// }
-
-// //Créer le tableau exercice
-// 	//créer tableau question
-// 		//encoder dans "question_info" les question UNE PAR UNE
-// 		//rajouter tableau réponse
-// 			//encore les réponse qui ont le meme id que la question
-
-// //Dans la fonction appel en AJAX
-// var session = new Session(JSON.parse(request.responsetext));
-
-// //Choisir l'id de l'exo à lancer
-
-// /*
-// Afficher réponse = Parcourir le tableau réponse et afficher dans la div si l'id de question correspond
-// Ajouter event onclic sur les réponse
-// */
-
-// var Session = function(resp){
-// 	this.data = resp;
-// 	this.lancer_exo = function(exoId, qId)
-// 	{
-// 		this.afficher_question(exoId, qId);
-// 		this.afficher_reponse(exoId, qId);
-// 	}
-// 	this.result = new Array();
-// 	this.qCount = 0;
-// 	this.rand = 0; //Si la session est random
-
-// }
-
-// session.prototype.afficher_question = function(exoId, qId)
-// {
-// 	document.getElementById("questionDisplay").innerHTML = this.data.exercice[exoId].question[qId].texte;
-// }
-
-// session.prototype.launch_random;
-
-// session.prototype.clickRep = function(aId, qId, exoId)
-// {
-// 	if(this.data.exercice[exoId].question[qId].reponse[aId].result == 1)
-// 	{
-// 		this.result.push(qId);
-// 	}
-
-// 	this.qCount++;
-// 	//Enlever les event onclick
-// 	if(qCount == data.totalQ){
-// 		this.finir();
-// 	}
-// 	else
-// 	{
-// 		if(!this.random)
-// 		{
-// 			if(qId == this.data.exercice[exoId].question.length - 1)
-// 				this.lancer_exo(exoId+1,0);
-// 		}
-// 		else
-// 		{
-// 			this.lancer_exo(exoId,qId+1);
-// 		}
-// 		// else
-// 		// {
-// 		// 	this.launch_random();
-// 		// }
-// 	}
-// }
